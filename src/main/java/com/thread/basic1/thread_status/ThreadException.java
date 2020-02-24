b9372c0f698bc754ce5246f9b674bb604f5fdd99
yang lee <liyang19900109@gmail.com>
2020-02-25 03:22:28 +0400

    线程的interruptException异常复位

--- src/main/java/com/thread/basic1/thread_status/ThreadException.java
+++ src/main/java/com/thread/basic1/thread_status/ThreadException.java
@@ -4,13 +4,38 @@
   */
  package com.thread.basic1.thread_status;
 
+ import java.util.concurrent.TimeUnit;
+
  /**
-  * @Description
+  * @Description todo 线程的异常中断
   * @Author YangLee
   * @Date2020/2/25 3:03 上午
   * @Version
   **/
  public class ThreadException {
 
+     private static int i;
 
+     public static void main(String[] args) throws InterruptedException {
+         Thread thread = new Thread(() -> {
+             while (!Thread.currentThread().isInterrupted()) {//默认是false  _interrupted state?
+
+                try {
+                    TimeUnit.SECONDS.sleep(10); //中断一个处于阻塞状态的线程。join/wait/queue.take..
+                    System.out.println("demo");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    break;
+                }
+                 i++;
+             }
+             System.out.println("i:" + i);
+         });
+         thread.start();
+         System.out.println("睡2秒之前");
+         TimeUnit.SECONDS.sleep(2);
+         thread.interrupt(); //把isInterrupted设置成true
+
+         System.out.println(thread.isInterrupted()); //true
+     }
  }
